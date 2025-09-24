6317bc7603226788f70c387d116bbefa4fdfe68c
Arjun Jhukal <jhukal9@gmail.com>
2025-09-24 15:04:49 +0545

    updated the exclusive game content back to live

--- src/app/(dashboard)/(user)/(outsideAuth)/exclusive-games/page.tsx
+++ src/app/(dashboard)/(user)/(outsideAuth)/exclusive-games/page.tsx
@@ -3,10 +3,8 @@ import React from 'react'
 
 export default function ExclusiveGames() {
     return (
-        // <ExclusiveGamePage />
-        <>
-            <h1>Exclusive Game</h1>
-        </>
+        <ExclusiveGamePage />
+
 
     )
 }
