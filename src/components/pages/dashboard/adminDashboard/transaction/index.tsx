5079ece0645ec41345c995085e9e55eae87c0c89
Arjun Jhukal <jhukal9@gmail.com>
2025-10-06 14:17:29 +0545

    updated the transaction table for dashboard as well

--- src/components/pages/dashboard/adminDashboard/transaction/index.tsx
+++ src/components/pages/dashboard/adminDashboard/transaction/index.tsx
@@ -1,12 +1,15 @@
+"use client"
+
 import PageHeader from '@/components/molecules/PageHeader'
 import React from 'react'
 import TransactionTable from './TransactionTable'
 
 export default function AllTransactionsPage() {
+    const [search, setSearch] = React.useState("");
     return (
         <>
             <PageHeader title='Tranactions' />
-            <TransactionTable />
+            <TransactionTable search={search} />
         </>
     )
 }
