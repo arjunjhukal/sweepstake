f75f34c41d3203cab24c4f707fa66ce5a254b9dd
Arjun Jhukal <jhukal9@gmail.com>
2025-10-09 16:15:59 +0545

    udpated the page redirection issue replace the listButton from mui to link component from react

--- src/components/organism/Sidebar/UserSidebar/PrimaryMenu.tsx
+++ src/components/organism/Sidebar/UserSidebar/PrimaryMenu.tsx
@@ -4,6 +4,7 @@ import { ReceiptEdit } from "@wandersonalwes/iconsax-react";
 import { PATH } from "@/routes/PATH";
 import { getAllMenus } from "@/serverApi/menu";
 import { useGetAllUserMenuQuery } from "@/services/menuApi";
+import Link from "next/link";
 
 export default function PrimaryMenu() {
     const { data } = useGetAllUserMenuQuery();
@@ -11,13 +12,19 @@ export default function PrimaryMenu() {
     return (
         <List>
             {data ? data?.data?.map((menu: any) => (
-                <ListItem key={menu.slug}>
-                    <ListItemButton component="a" href={menu.slug ? `/general/${menu.slug}` : "#"}>
+                <ListItem key={menu.name}>
+                    {/* <ListItemButton component="a" href={menu.slug ? `/general/${menu.slug}` : "#"}>
                         <ListItemIcon>
                             <ReceiptEdit size={18} />
                         </ListItemIcon>
                         <ListItemText primary={menu.name} />
-                    </ListItemButton>
+                    </ListItemButton> */}
+                    <Link href={menu.slug ? `/general/${menu.slug}` : "#"} className="flex gap-2 items-center py-2">
+                        <ListItemIcon>
+                            <ReceiptEdit size={18} />
+                        </ListItemIcon>
+                        <ListItemText primary={menu.name} />
+                    </Link>
                 </ListItem>
             ))
                 : (
