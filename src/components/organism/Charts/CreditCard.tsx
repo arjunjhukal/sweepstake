d9e80ef7ef2bc281859d5dbfb73f08e11ddded00
Arjun Jhukal <jhukal9@gmail.com>
2025-10-29 11:49:14 +0545

    updated the minor change

--- src/components/organism/Charts/CreditCard.tsx
+++ src/components/organism/Charts/CreditCard.tsx
@@ -31,6 +31,7 @@ const CreditCard = ({ game }: { game: any }) => {
         },
     ];
 
+    console.log(game)
     return (
         <div className=" rounded-lg p-3 border border-gray">
             <Image src={game?.logo || "/assets/images/auth-image.png"} alt='' width={32} height={32} className='aspect-square rounded-sm' />
@@ -47,8 +48,8 @@ const CreditCard = ({ game }: { game: any }) => {
                     width={"80%"}
                 />
                 <div className="flex justify-between text-xs mt-2" >
-                    <span>${game.amount}</span>
-                    <span>{game.percentage}%</span>
+                    <span>{game.type === "gc" ? "" : "$"}{game.amount}</span>
+                    {game.type === "gc" ? "" : <span>{game.percentage}%</span>}
                 </div>
             </div>
         </div>
